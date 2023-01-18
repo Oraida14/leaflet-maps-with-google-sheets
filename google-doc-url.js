167fdaa656a43021d50e028b9bdf5ee3f73addb9
Oraida14 <116577694+Oraida14@users.noreply.github.com>
2023-01-17 19:27:21 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,4 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wu4s3yCKCNtCQqj333PSgKFfiLwTu6sLq6eF-c6iAw4/edit#gid=0';
 
-// insert your own Google Sheets API key from https://console.developers.google.cor googleApiKey = 'AIzaSyAGg886BL2UKTt6YksBRH2734sljRX-F9k';
+// insert your own Google Sheets API key from https://console.developers.google.cor 
+var googleApiKey = 'AIzaSyAGg886BL2UKTt6YksBRH2734sljRX-F9k';
